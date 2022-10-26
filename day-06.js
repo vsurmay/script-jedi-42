f4166926785de1f544b486bb51c4431844b4d1eb
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-10-26 22:33:36 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,1 +1,18 @@
+//
+//
 
+//Is he gonna survive?
+//https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+function hero(bullets, dragons) {
+    if (bullets / 2 >= dragons) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//5 without numbers:
+//https://www.codewars.com/kata/59441520102eaa25260000bf
+function unusualFive() {
+  const str = 'fffff';
+  return str.length;
+}
