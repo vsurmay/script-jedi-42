deabed54c30954a0c0519f3a0d33aea12a6d961d
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-10-29 13:23:30 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,9 +1,7 @@
 //Syntax task
 //https://www.codewars.com/kata/is-this-my-tail/train/javascript
 function correctTail(bod, tail) {
-  
   bod = bod.substr(bod.length-(tail.length));
-  
   if (bod === tail) {
     return true;
   } else {
@@ -13,11 +11,11 @@ function correctTail(bod, tail) {
 //Do I get a bonus
 //https://www.codewars.com/kata/56f6ad906b88de513f000d96
 function bonusTime(salary, bonus) {
-    if (bonus) {
-        return '£' + (salary * 10);
-    } else {
-        return '£' + salary;
-    }
+  if (bonus) {
+    return '£' + (salary * 10);
+  } else {
+    return '£' + salary;
+  }
 }
 //Automorphic number
 //https://www.codewars.com/kata/5a58d889880385c2f40000aa
