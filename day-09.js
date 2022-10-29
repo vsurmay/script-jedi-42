ed1fa382e481d92c4801bc44aa55f360093241ed
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-10-29 13:28:55 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,18 +1,18 @@
 //#16: Methods of String object--slice(), substring() and substr()
 //http://www.codewars.com/kata/57274562c8dcebe77e001012
 function cutIt(arr) {
-    const arrLength = [];
-    for (const i of arr) {
-        if (i.length > 0) {
-            arrLength.push(i.length);
-        }
-    }
-    const arrLengthMin = Math.min(...arrLength);
-    const arrResult = [];
-    for (const i of arr) {
-        if (i.length >= arrLengthMin) {
-            arrResult.push(i.slice(0, arrLengthMin));
-        }
-    }
-    return arrResult
+  const arrLength = [];
+  for (const i of arr) {
+      if (i.length > 0) {
+          arrLength.push(i.length);
+      }
+  }
+  const arrLengthMin = Math.min(...arrLength);
+  const arrResult = [];
+  for (const i of arr) {
+      if (i.length >= arrLengthMin) {
+          arrResult.push(i.slice(0, arrLengthMin));
+      }
+  }
+  return arrResult;
 }
