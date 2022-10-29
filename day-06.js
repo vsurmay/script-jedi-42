07236b28ad91e97af240dbe8b9eec68ccd746f90
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-10-29 13:25:08 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,17 +1,17 @@
 //Maximum Multiple
 //https://www.codewars.com/kata/maximum-multiple/train/javascript
 function maxMultiple(divisor, bound) {
-    const n = Math.floor(bound / divisor);
-    return n * divisor
+  const n = Math.floor(bound / divisor);
+  return n * divisor;
 }
 //Is he gonna survive?
 //https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
 function hero(bullets, dragons) {
-    if (bullets / 2 >= dragons) {
-        return true;
-    } else {
-        return false;
-    }
+  if (bullets / 2 >= dragons) {
+      return true;
+  } else {
+      return false;
+  }
 }
 //5 without numbers:
 //https://www.codewars.com/kata/59441520102eaa25260000bf
