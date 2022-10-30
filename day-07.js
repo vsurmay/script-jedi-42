1e35ef69a52097f6c902d6823956ac7a24ad13e0
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-10-30 15:49:14 +0200

    Update day-07.js

--- day-07.js
+++ day-07.js
@@ -1,1 +1,19 @@
-
+//[JEDI LEVEL] Error Throwing - Error Handling #2
+//https://www.codewars.com/kata/55e7650c8d894146be000095/
+function validateMessage(msg) {
+  if (msg === null) {
+    throw new ReferenceError("Message is null!");
+  }
+  if (typeof msg != "string") {
+    throw new TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`
+    );
+  }
+  if (msg.includes("<") && msg.includes(">")) {
+    return false;
+  }
+  if (msg.length === 0 || msg.length > 255) {
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  }
+  return true;
+}
