a9ea4f5eb17f4dcf605a9c11592886d56a06751d
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-10-31 18:07:33 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -1,1 +1,30 @@
+//Playing with Sets : Equal or Not ?
+//https://www.codewars.com/kata/playing-with-sets-equal-or-not/train/javascript
+function areEqual(s1, s2) {
+  if (s1.size !== s2.size) return false;
+  const result = new Set();
+  for (const i of s1) {
+    result.add(i);
+  }
+  for (const i of s2) {
+    result.add(i);
+  }
+  if (result.size === s1.size && result.size === s2.size) {
+    return true;
+  } else return false;
+}
+
+function notEqual(s1, s2) {
+  if (s1.size !== s2.size) return true;
+  const result = new Set();
+  for (const i of s1) {
+    result.add(i);
+  }
+  for (const i of s2) {
+    result.add(i);
+  }
+  if (result.size === s1.size && result.size === s2.size) {
+    return false;
+  } else return true;
+}
 
