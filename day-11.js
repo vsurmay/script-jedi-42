28dd23dc914a02eebbfb9f2f3af97d212ec609a9
unknown <vsurmaj510@gmail.com>
2022-11-01 12:30:37 +0200

    aded task

--- day-11.js
+++ day-11.js
@@ -17,3 +17,28 @@ function areEqual(s1, s2) {
 function notEqual(s1, s2) {
   return !areEqual(s1, s2);
 }
+//[JEDI LEVEL] Operations with Sets
+//https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
+function process2Arrays(arr1, arr2) {
+  const lengthArr1 = arr1.length;
+  const lengthArr2 = arr2.length;
+  const sumLengthArr = lengthArr1 + lengthArr2;
+  const setArray = new Set();
+  for (const i of arr1) {
+    setArray.add(i);
+  }
+  for (const i of arr2) {
+    setArray.add(i);
+  }
+  const sizeSetArray = setArray.size;
+  const repeatSymbol = sumLengthArr - sizeSetArray;
+  const unrepeatableSymbol = sizeSetArray - repeatSymbol;
+  const unrepeatableSymbolArr1 = lengthArr1 - repeatSymbol;
+  const unrepeatableSymbolArr2 = lengthArr2 - repeatSymbol;
+  return [
+    repeatSymbol,
+    unrepeatableSymbol,
+    unrepeatableSymbolArr1,
+    unrepeatableSymbolArr2,
+  ];
+}
