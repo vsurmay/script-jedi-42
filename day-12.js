e5649884b40025372eacdc8ced262a96f0e2002f
unknown <vsurmaj510@gmail.com>
2022-11-01 12:58:20 +0200

    change word binnary

--- day-12.js
+++ day-12.js
@@ -1,14 +1,14 @@
 //Word to Binary
 //https://www.codewars.com/kata/59859f435f5d18ede7000050
 function wordToBin(str) {
-  const s = str.split("");
+  const arrayStr = str.split("");
   const result = [];
-  for (const i of s) {
-    let k = i.charCodeAt().toString(2);
-    if (k.length < 8) {
-      k = "0" + k;
+  for (const i of arrayStr) {
+    let kods = i.charCodeAt().toString(2);
+    if (kods.length < 8) {
+      kods = "0" + kods;
     }
-    result.push(k);
+    result.push(kods);
   }
   return result;
 }
