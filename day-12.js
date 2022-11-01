057b7fa597a9acb28d978a4cb6d061dc5944e2db
unknown <vsurmaj510@gmail.com>
2022-11-01 15:20:32 +0200

    aded Jedi task

--- day-12.js
+++ day-12.js
@@ -12,3 +12,34 @@ function wordToBin(str) {
   }
   return result;
 }
+//[JEDI LEVEL] Char Codes Calculation
+//https://www.codewars.com/kata/57f75cc397d62fc93d000059
+function calc(x) {
+  const array = x.split("");
+  const result = [];
+  for (const i of array) {
+    result.push(i.charCodeAt());
+  }
+  const total1 = result.join("");
+  const total1Array = total1.toString().split("");
+  const result2 = [];
+  for (const i of total1Array) {
+    if (i === "7") {
+      const number = 1;
+      result2.push(number);
+    } else {
+      result2.push(i);
+    }
+  }
+  const total2 = result2.join("");
+  const total2Array = total2.toString().split("");
+  let total1Sum = 0;
+  for (const i of total1Array) {
+    total1Sum += Number(i);
+  }
+  let total2Sum = 0;
+  for (const i of total2Array) {
+    total2Sum += Number(i);
+  }
+  return total1Sum - total2Sum;
+}
