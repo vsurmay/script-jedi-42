e9b2890b0225f97f86b9208867e8115b1922f783
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-03 16:37:10 +0200

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -1,10 +1,10 @@
-//
-//
-function shuffleIt(arr, ...swapArr) {
-  for (let i = 0; i < swapArr.length; i++) {
-    let firstNum = swapArr[i][0];
-    let secondNum = swapArr[i][1];
-    [arr[firstNum], arr[secondNum]] = [arr[secondNum], arr[firstNum]];
+//#22: Unlock new skills--Arrow function,spread operator and deconstruction
+//http://www.codewars.com/kata/572ab0cfa3af384df7000ff8
+function shuffleIt(arr, ...arrays) {
+  for (let i = 0; i < arrays.length; i++) {
+    let numFirst = arrays[i][0];
+    let numSecond = arrays[i][1];
+    [arr[numFirst], arr[numSecond]] = [arr[numSecond], arr[numFirst]];
   }
   return arr;
 }
