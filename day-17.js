a3182f12d23f21015c175719fe268ac2b1b370cf
unknown <vsurmaj510@gmail.com>
2022-11-06 18:10:01 +0200

    aded 29 task

--- day-17.js
+++ day-17.js
@@ -22,3 +22,13 @@ function countGrade(scores) {
     }).length,
   };
 }
+//
+//
+
+//#29: methods of arrayObject---concat() and join()
+//http://www.codewars.com/kata/5731861d05d14d6f50000626
+function bigToSmall(arr) {
+  const newArr = [].concat(...arr);
+  newArr.sort((a, b) => b - a);
+  return newArr.join(">");
+}
