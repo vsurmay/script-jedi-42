d7ac2116ecfd0dc30fa8cbc97170fe8f8d48d234
unknown <vsurmaj510@gmail.com>
2022-11-06 15:48:26 +0200

    aded task

--- day-16.js
+++ day-16.js
@@ -15,3 +15,39 @@ function threeInOne(arr) {
   }
   return result;
 }
+//[JEDI LEVEL] #25: methods of arrayObject---reverse() and sort()
+//http://www.codewars.com/kata/572df796914b5ba27c000c90
+function sortIt(arr) {
+  const copyArr = [...arr];
+  copyArr.sort((a, b) => {
+    let numa = 0;
+    let numb = 0;
+    for (i of arr) {
+      if (i === a) numa++;
+      if (i === b) numb++;
+    }
+    if (numa > numb) return 1;
+    if (numa < numb) return -1;
+    if (numa === numb) return b - a;
+  });
+  return copyArr;
+}
+//#26: methods of arrayObject---map()
+//http://www.codewars.com/kata/572fdeb4380bb703fc00002c
+function isolateIt(arr) {
+  let result = arr.map((item) => {
+    let n = Math.floor(item.length / 2);
+    if (item.length % 2) {
+      const itemArr = item.split("");
+      itemArr.splice(n, 1, "|");
+      itemStr = itemArr.join("");
+      return itemStr;
+    } else {
+      const itemArr = item.split("");
+      itemArr.splice(n, 0, "|");
+      itemStr = itemArr.join("");
+      return itemStr;
+    }
+  });
+  return result;
+}
