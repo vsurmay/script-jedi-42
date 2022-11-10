00a99982669ebd5cdeb04e88e7cee95e22386b43
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-10 13:44:26 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,9 @@
+//#39: Regular Expression--""?"", ""*"", ""+"" and ""{}""
+//https://www.codewars.com/kata/573bca07dffc1aa693000139
+
+const regex=/^(9|-9)\d*0{4,}$/
+
+//#40: Regular Expression--""|"", ""[]"" and ""()"" 
+//http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+const regex= /https?:\/\/[\w.]+\.(com|net)/gi
