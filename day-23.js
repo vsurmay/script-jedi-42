07ab3f78967b6c59232cc315a62d1bf8fdc321a1
unknown <vsurmaj510@gmail.com>
2022-11-11 20:21:05 +0200

    aded first task

--- day-23.js
+++ day-23.js
@@ -1,5 +1,9 @@
-//
-//
+//Unpacking arguments
+//http://www.codewars.com/kata/unpacking-arguments
+
+function spread(func, args) {
+  return func(...args);
+}
 
 //For the sake of argument
 //http://www.codewars.com/kata/for-the-sake-of-argument
