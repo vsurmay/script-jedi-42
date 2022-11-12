0a2c0f0abeae031ef4c6d2572b6d283f4b4f74be
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-12 13:04:50 +0200

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -2,3 +2,9 @@
 //http://www.codewars.com/kata/573e6831e3201f6a9b000971
 
 const regex=/\b(\w)(\w)?(\w)?\w?\3\2\1\b/g;
+
+//[JEDI LEVEL]  #42: Regular Expression--(?:), (?=) and (?!)
+//http://www.codewars.com/kata/573fb9223f9793e485000453
+
+const regex = /(\d)(?=(\d\d\d)+(?!\d))/g;
+const addCommas = (money, reg) => money.replace(reg, (x) => x + ",");
