b66b8551c30fc413df1eab69a1a4d7d51e26615a
unknown <vsurmaj510@gmail.com>
2022-11-12 17:54:32 +0200

    aded jedi task

--- day-24.js
+++ day-24.js
@@ -6,3 +6,16 @@ function solution(...arg) {
   const result = arg.length === argSet.size ? false : true;
   return result;
 }
+
+//[JEDI LEVEL] Last Argument
+//http://www.codewars.com/kata/last
+function last(...list) {
+  const result = list[list.length - 1];
+  if (Array.isArray(result)) {
+    return result[result.length - 1];
+  } else if (typeof result === "string") {
+    return result.slice(-1);
+  } else {
+    return result;
+  }
+}
