801b057d34a10c6e36c61b056147238c1e3024fa
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-16 17:51:37 +0200

    Update day-27.js

--- day-27.js
+++ day-27.js
@@ -15,3 +15,12 @@ function buildFun(n) {
 }
 //Shifty closures
 //https://www.codewars.com/kata/shifty-closures/train/javascript
+const greet_abe = function () {
+  const name = "Abe";
+  return "Hello, " + name + "!";
+};
+
+const greet_ben = function () {
+  const name = "Ben";
+  return "Hello, " + name + "!";
+};
