dbd70326a143ef2ab5e3b1b365f05da4e9928f9e
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-16 17:25:31 +0200

    Create day-27.js

---
+++ day-27.js
@@ -0,0 +1,17 @@
+//Understanding closures - the basics 
+//https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+function buildFun(n) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(
+      (function (i) {
+        return function () {
+          return i;
+        };
+      })(i)
+    );
+  }
+  return res;
+}
+//Shifty closures
+//https://www.codewars.com/kata/shifty-closures/train/javascript
