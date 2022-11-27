fa4a8a3f83d63735a37b3bade65cd0ddca3566a3
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-27 22:11:28 +0200

    Update day-25.js

--- day-25.js
+++ day-25.js
@@ -1,14 +1,17 @@
 //Anonymous returns
 //http://www.codewars.com/kata/anonymous-returns
 const name = "The Window";
+
 const alpha = {
   name: "My Alpha",
   getNameFunc: function () {
+    const context = this;
     return function () {
-      return alpha.name;
+      return context.name;
     };
   },
 };
+
 //Take an Arrow to the knee, Functionally
 //https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
 const ArrowFunc = arr => arr.map(el => String.fromCharCode(el)).join('');
