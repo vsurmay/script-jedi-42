9f8e2b696852c151f795b5c77a5657a162ac5112
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-11-28 14:06:38 +0200

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -1,4 +1,16 @@
-//
-//
+//How new works
+//https://www.codewars.com/kata/how-new-works
 const myObj = Object.create(MyObject.prototype);
-MyObject.bind(myObj)()
+MyObject.bind(myObj)();
+
+//Replicate new
+//http://www.codewars.com/kata/replicate-new
+function nouveau (Constructor, ...args) {
+  const inst = Object.create(Constructor.prototype);
+  const result = Constructor.apply(inst, args);
+  if (result === Object(result)) {
+    return result;
+  } else {
+    return inst;
+  }
+}
