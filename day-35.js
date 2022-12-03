42c038dea5d72f49643927c65a1804f4dea2c367
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-12-03 16:06:18 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -1,3 +1,14 @@
 //
 //
 Object.prototype.description = "TODO: GIVE ME A DESCRIPTION";
+//
+//
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`;
+  }
+}
