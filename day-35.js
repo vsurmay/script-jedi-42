30b723cb2c4cbdd41d084d7282f52e9a3c519b39
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-12-03 16:18:25 +0200

    Update day-35.js

--- day-35.js
+++ day-35.js
@@ -1,8 +1,10 @@
-//
-//
+//Object Prototypes
+//https://www.codewars.com/kata/js-prototypes-module-number-1-object-prototypes
+
 Object.prototype.description = "TODO: GIVE ME A DESCRIPTION";
-//
-//
+//Class-Like Objects
+//https://www.codewars.com/kata/javascript-class-like-objects
+
 class Animal {
   constructor(name, type) {
     this.name = name;
@@ -12,3 +14,16 @@ class Animal {
     return `${this.name} is a ${this.type}`;
   }
 }
+//[JEDI LEVEL] Power of bind
+//http://www.codewars.com/kata/power-bind
+
+Function.prototype.bind = function (ctx) {
+const fn = this;
+  return function(...args) {
+    if (this === global) {
+      return fn.call(ctx, args);
+    } else {
+      return fn.call(this, args);
+    }  
+  }
+};
