605369a2e8d206d75779b77076639288ea0d543c
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-12-05 14:15:34 +0200

    Update day-33.js

--- day-33.js
+++ day-33.js
@@ -1,5 +1,5 @@
-//
-//
+//[JEDI LEVEL] Implement own reduce
+//https://www.codewars.com/kata/array-number-reduce/train/javascript
 Array.prototype.reduce = function (process, initial) {
   let arr = this;
   let acc = initial || arr[0];
@@ -7,4 +7,10 @@ Array.prototype.reduce = function (process, initial) {
     acc = process(acc, arr[i], i);
   }
   return acc;
+};
+//[JEDI LEVEL] Deep freeze
+//https://www.codewars.com/kata/52ed80dfe7bf9ae61d000056
+Object.deepFreeze = function (object) {
+  Object.freeze(object);
+  for (let key in object) Object.deepFreeze(object[key]);
 };
