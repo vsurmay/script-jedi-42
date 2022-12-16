ec9e50a4d3a18b7b089d298dc8f7f67ba4bcbc01
vsurmay <112492964+vsurmay@users.noreply.github.com>
2022-12-16 17:24:58 +0200

    Update day-42.js

--- day-42.js
+++ day-42.js
@@ -1,5 +1,5 @@
-//
-//
+//Scramblies
+//https://www.codewars.com/kata/55c04b4cc56a697bb0000048
 function scramble(str1, str2) {
   const arr1 = [...str1];
   const arr2 = [...str2];
@@ -15,4 +15,12 @@ function scramble(str1, str2) {
     if (obj2[key] > obj1[key] || obj1[key] == undefined) return false;
   }
   return true;
+}
+//[JEDI LEVEL] Two Sum
+//https://leetcode.com/problems/two-sum/
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const indexSecondNum = nums.indexOf(target - nums[i], i + 1);
+    if (secondNum !== -1) return [i, indexSecondNum];
+  }
 }
